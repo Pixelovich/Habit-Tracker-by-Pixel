e8fd8d76f7c20fc5664aa9a77a6e7b7e671e47bb
Pixelovich <luis.lecumberri@hotmail.com>
2026-09-18 11:54:50 +0000

    Fix Supabase storage for web

--- app/HabitTracker/src/services/supabase.ts
+++ app/HabitTracker/src/services/supabase.ts
@@ -1,6 +1,5 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
-import 'expo-sqlite/localStorage/install';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabasePublishableKey =
